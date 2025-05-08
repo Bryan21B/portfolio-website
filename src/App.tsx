7554ded2fa8cbb3c56501518355fa9215314f331
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-05-08 15:53:54 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Home</div>;
+      <div>Home</div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import App from "./App.tsx";
+import NotFound from "./pages/NotFound.tsx";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
@@ -10,7 +11,8 @@ createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route index path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
