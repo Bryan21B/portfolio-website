f38cd83ba674a2de2c78ab353e14df0365e716b7
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-05-08 15:50:18 +0200

    Fix ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -9,11 +9,14 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     plugins: { js },
     extends: ["js/recommended"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
 ]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import "./index.css";
 
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import App from "./App.tsx";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
-  // @ts-ignore
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
